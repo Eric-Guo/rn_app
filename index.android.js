2291a3e5869b66ddf3d945ccf4c58efe106d3082
Eric Guo <eric.guo@sandisk.com>
2015-11-30 22:49:37 +0800

    Fetching real data

--- index.android.js
+++ index.android.js
@@ -8,14 +8,54 @@ import React, {
   View
 } from 'react-native';
 
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 class rn_app extends Component {
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+  getInitialState: function() {
+    return {
+      movies: null,
+    };
+  },
 
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMovie: function(movie) {
     return (
       <View style={styles.container}>
         <Image
