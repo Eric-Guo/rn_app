d16f34bf2b298bd689c0bacbc6dc12c467ac1324
Eric Guo <eric.guo@sandisk.com>
2015-11-30 22:41:43 +0800

    Add some styling

--- index.android.js
+++ index.android.js
@@ -2,19 +2,58 @@
 import React, {
   AppRegistry,
   Component,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const Hello = require('./components/Hello');
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
 class rn_app extends Component {
-  render() {
+  render: function() {
+    var movie = MOCKED_MOVIES_DATA[0];
+
     return (
-      <Hello />
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
     );
   }
+}
+
+class styles extends StyleSheet {
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
 }
 
 AppRegistry.registerComponent('rn_app', () => rn_app);
