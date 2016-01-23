48043c4c108b1218ce313c2f6d7f9e6fec4ad5c1
Eric Guo <eric.guo@sandisk.com>
2016-01-23 22:36:28 +0800

    update the new init rn_app

--- components/Hello.js
+++ components/Hello.js
@@ -11,8 +11,14 @@ module.exports = React.createClass({
 
     return (
       <View style={styles.container}>
-        <Text>
-          Hello World!
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
         </Text>
       </View>
     );
@@ -22,6 +28,18 @@ module.exports = React.createClass({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  }
 });
--- index.ios.js
+++ index.ios.js
@@ -1,43 +1,20 @@
 'use strict';
-
-const React = require('react-native');
-const {
+import React, {
   AppRegistry,
+  Component,
   StyleSheet,
   Text,
-  View,
-} = React;
+  View
+} from 'react-native';
 
 const Hello = require('./components/Hello');
 
-var rn_app = React.createClass({
-  render: function() {
+class rn_app extends Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Hello />
-      </View>
+      <Hello />
     );
   }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    marginTop: 20,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
 
 AppRegistry.registerComponent('rn_app', () => rn_app);
